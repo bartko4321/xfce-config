6862a6d9b8ec7938257854e6cce6e54d8899fe4e
Bartosz Szczeciński <54596452+bartko4321@users.noreply.github.com>
2026-08-14 20:03:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # ==========================================================
 # KOMPLEKSOWY SKRYPT KONFIGURACYJNY SYSTEMU (XFCE)
-# Z wbudowanym ukrywaniem logów, paskiem postępu i obsługą błędów.
 # ==========================================================
 
 set -euo pipefail
